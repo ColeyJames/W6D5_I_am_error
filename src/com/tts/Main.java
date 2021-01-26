866b087a3802406344724b590acc94c86379de93
ColeyJames <coleyj777@yahoo.com>
2021-01-26 13:43:51 -0600

    first commit

---
+++ src/com/tts/Main.java
@@ -0,0 +1,22 @@
+package com.tts;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int treecko = 252;
+        System.out.print("Third gen rocks! " + treecko + "\n");
+    boolean sudowoodo = false;
+        System.out.print("This looks like a tree! " + sudowoodo);
+    //boolean trying to be declared as an int
+    //sudowoodo = 185;
+    //incompatible types: int cannot be converted to boolean
+
+//    //What not to do...
+//    int sceptile = 254;
+//        public class evolution {
+//            int treecko + 2 = int sceptile
+//        }
+//    //java: illegal start of expression
+    }
+}
